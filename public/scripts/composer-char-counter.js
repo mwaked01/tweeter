5aec93b8bfecf7ac09e172290395356d8c2dff2f
Mohamed Waked <mwaked01@gmail.com>
2023-09-04 16:48:34 -0400

    add avatar and tweet box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
-  $("#tweet-text").on("input", function () {
+  $("#tweet-text").on("input", function() {
     const $counter = $(this).closest('form').find('.counter');
-    const chrasLeft = 140-this.value.length;
+    const chrasLeft = 140 - this.value.length;
 
-    if (chrasLeft < 0){
+    if (chrasLeft < 0) {
       $counter.addClass('negative');
     } else {
       $counter.removeClass('negative');
     }
     
     $counter.text(chrasLeft);
-  })
+  });
 });
