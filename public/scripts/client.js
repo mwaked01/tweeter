193222850e38f2338db89d364cb7f375d4e0684d
Mohamed Waked <mwaked01@gmail.com>
2023-09-10 19:06:30 -0400

    implement Form Validation with alerts

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-  const renderTweets = function(tweets) {
+$(document).ready(function () {
+  const renderTweets = function (tweets) {
     for (let tweet of tweets) {
       $('#tweets-container').append(createTweetElement(tweet));
     }
   };
 
-  const createTweetElement = function(tweet) {
+  const createTweetElement = function (tweet) {
     const $tweet = $(`<article class="tweet">
   <header>
   <p><img src="${tweet['user'].avatars}">${tweet['user'].name}</p>
@@ -26,20 +26,29 @@ $(document).ready(function() {
   };
 
   const $form = $('#tweet-form');
+  
   $form.submit((event) => {
+    let $tweetText = $("#tweet-text").val();
     //stop default submit from refreshing the page
     event.preventDefault();
-    $.ajax({
-      url: "/tweets",
-      method: "POST",
-      data: $form.serialize(),
-    });
+    if ($tweetText === "" || $tweetText === null) {
+      alert("Invalid Entry!");
+    } else if ($tweetText.length > 140) {
+      alert("Tweet Exceeds limit of 140 characters.");
+    } else {
+      $.ajax({
+        url: "/tweets",
+        method: "POST",
+        data: $form.serialize(),
+      });
+
+    }
   });
 
-  const loadTweets = function() {
+  const loadTweets = function () {
     const tweetsPage = 'http://localhost:8080/tweets';
     $.ajax(tweetsPage, { method: 'GET' })
-      .then(function(tweets) {
+      .then(function (tweets) {
         console.log('Success: ', tweets);
         renderTweets(tweets);
       });
