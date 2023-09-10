8324d5ef84a3cd47425b2a7857e504186a8371a6
Mohamed Waked <mwaked01@gmail.com>
2023-09-10 15:34:57 -0400

    Format tweet time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,33 +1,4 @@
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
 $(document).ready(function() {
-
   const renderTweets = function(tweets) {
     for (let tweet of tweets) {
       $('#tweets-container').append(createTweetElement(tweet));
@@ -42,7 +13,7 @@ $(document).ready(function() {
         </header>
         ${tweet['content'].text}
         <footer>
-        ${tweet['created_at']}
+        ${timeago.format(tweet['created_at'])}
           <div class="icons">
             <i class="fa-solid fa-flag " ></i>
             <i class="fa-solid fa-retweet"></i>
@@ -53,9 +24,6 @@ $(document).ready(function() {
         <br>`);
     return $tweet;
   };
-
-  //renderTweets(data);
-
 
   const $form = $('#tweet-form');
   $form.submit((event) => {
