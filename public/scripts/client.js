cb8724eaf65dbca7bc7e5bd337c101e1ca961a28
Mohamed Waked <mwaked01@gmail.com>
2023-09-10 14:53:24 -0400

    implement AJAX submit request

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,36 +1,32 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
 // Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
 
 $(document).ready(function () {
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
 
   const renderTweets = function (tweets) {
     for (let tweet of tweets) {
@@ -58,11 +54,29 @@ $(document).ready(function () {
     return $tweet;
   };
 
-  renderTweets(data);
+  //renderTweets(data);
 
-  // const $tweet = createTweetElement(tweetData);
 
-  // // Test / driver code (temporary)
-  // console.log($tweet); // to see what it looks like
-  // $('#tweets-container').append($tweet);
+  const $form = $('#tweet-form');
+  $form.submit((event) => {
+    //stop default submit from refreshing the page
+    event.preventDefault();
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: $form.serialize(),
+    });
+  });
+
+  const loadTweets = function (){
+    const tweetsPage = 'http://localhost:8080/tweets';
+    $.ajax(tweetsPage, { method: 'GET' })
+    .then(function (tweets) {
+      console.log('Success: ', tweets);
+      renderTweets(tweets);
+    });
+  }
+
+loadTweets();
+
 });
